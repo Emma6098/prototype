976696718137eec8aa78d8c0ea4f6f4750147383
Emma Creteur <emma_creteur@hotmail.com>
2023-11-30 17:06:24 +0100

    controller users + reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,26 @@
 class ReviewsController < ApplicationController
+
+  def create
+    @review = Review.new(review_params)
+    @user = User.find(params[:user_id])
+    @review.user = current_user
+    if @review.save
+    redirect_to user_path(@user)
+    else
+      render 'users/show', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to user_path(@review.user), status: see_other
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:comment, :rating)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+    @review = Review.new(user: @user)
+    @reviews = @user.reviews
+
+    total_rating = @reviews.sum(&:rating)
+    @average_rating = (total_rating.to_f / @reviews.count).round(1) unless @reviews.empty?
+  end
+
 end
