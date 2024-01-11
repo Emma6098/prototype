64f48c1bb0b64971e71cca40ac638e1d25e46a35
Emma Creteur <emma_creteur@hotmail.com>
2024-01-11 16:15:07 +0100

    ajout logo + essaie js view new

--- app/javascript/controllers/brand_controller.js
+++ app/javascript/controllers/brand_controller.js
@@ -1,14 +1,24 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["brand", "autreMarqueInput"];
+  static targets = ["brandInput", "autreMarqueInput"];
 
   connect() {
+    console.log("Hello from brand controller");
     this.toggleAutreMarqueInput();
+    this.addBrandChangeListener();
   }
 
+  addBrandChangeListener() {
+      const brandSelect = this.brandInputTarget.querySelector('select');
+
+      brandSelect.addEventListener('change', () => {
+        this.toggleAutreMarqueInput();
+      });
+    }
+
   toggleAutreMarqueInput() {
-    const selectedBrand = this.brandTarget.value;
+    const selectedBrand = this.brandInputTarget.querySelector('select').value;
     if (selectedBrand === "Autre marque") {
       this.autreMarqueInputTarget.classList.remove("hidden");
     } else {
@@ -16,22 +26,4 @@ export default class extends Controller {
     }
   }
 
-  document.addEventListener('DOMContentLoaded', function () {
-    var brandDropdown = document.getElementById('article_brand');
-    var autreMarqueInput = document.getElementById('autreMarqueInput');
-
-    // Gérer la visibilité initiale lors du chargement de la page
-    if (brandDropdown.value === 'Autre marque') {
-      autreMarqueInput.classList.remove('hidden');
-    }
-
-    // Ajouter un écouteur d'événements pour détecter les changements dans le menu déroulant
-    brandDropdown.addEventListener('change', function () {
-      if (brandDropdown.value === 'Autre marque') {
-        autreMarqueInput.classList.remove('hidden');
-      } else {
-        autreMarqueInput.classList.add('hidden');
-      }
-    });
-  });
 }
